e244a987f0ce246a2f6c56ec67863d964da809e0
 Liza <liza@MacBook-Air-Liza.local>
2024-10-21 13:37:27 +0200

    init Freelancer Service

---
+++ freelancer/src/test/java/com/arkhanhelskaya/freelancer/FreelancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arkhanhelskaya.freelancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
